81ae5d9471f22fef97961774b94929770f80dcda
Victor Otto <56771653+Kzapka@users.noreply.github.com>
2020-08-19 23:27:08 -0300

    Deletar
    Adicionado o delete do banco

---
+++ src/main/java/aula20200819/delete/Imovel.java
@@ -0,0 +1,33 @@
+package aula20200819.delete;
+
+public class Imovel {
+	private Long id;
+	private String descrição;
+	private Double metragem;
+
+	public Imovel(Long id, String descrição, Double metragem) {
+		this(descrição, metragem);
+		this.id = id;		
+	}
+
+	public Imovel(String descrição, Double metragem) {
+		this.descrição = descrição;
+		this.metragem = metragem;
+	}
+	
+	public String getDescricao() {
+		return descrição;
+	}
+	public Double getMetragem() {
+		return metragem;
+	}
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Imóvel [id=" + id + ", descrição=" + descrição + ", metragem=" + metragem + "]";
+	}
+	
+}
